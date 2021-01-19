3662a43aade1c1637c234749a798eedaa03b1933
Ben Attali <ben.attali8@gmail.com>
2021-01-18 23:04:34 -0500

    reverse string in place complete

---
+++ arrays/reverse_string_in_place.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+def reverse(list_of_chars):
+
+    loop_length = len(list_of_chars) - 1
+    
+    for i in range(loop_length):
+        index_to_move = -2 - i
+        char_to_move = list_of_chars.pop(index_to_move)
+        list_of_chars.append(char_to_move)
+    
+    return(list_of_chars)
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_empty_string(self):
+        list_of_chars = []
+        reverse(list_of_chars)
+        expected = []
+        self.assertEqual(list_of_chars, expected)
+
+    def test_single_character_string(self):
+        list_of_chars = ['A']
+        reverse(list_of_chars)
+        expected = ['A']
+        self.assertEqual(list_of_chars, expected)
+
+    def test_longer_string(self):
+        list_of_chars = ['A', 'B', 'C', 'D', 'E']
+        reverse(list_of_chars)
+        expected = ['E', 'D', 'C', 'B', 'A']
+        self.assertEqual(list_of_chars, expected)
+
+
+unittest.main(verbosity=2)
