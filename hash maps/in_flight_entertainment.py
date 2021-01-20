00e09780e0cc24e2c54f62596b3c26894c82335f
Ben Attali <ben.attali8@gmail.com>
2021-01-20 13:17:32 -0500

    in flight entertainment complete - bad time complexity O(n^2). need to look at this again

---
+++ hash maps/in_flight_entertainment.py
@@ -0,0 +1,61 @@
+import unittest
+
+
+def can_two_movies_fill_flight(movie_lengths: list, flight_length: int) -> bool:
+    if not movie_lengths:
+        return False
+
+    movie_one_length = movie_lengths[0]
+    i = 0
+
+    while i < len(movie_lengths) - 1:
+        for length in movie_lengths[i+1:]:
+            if movie_one_length + length == flight_length:
+                return True
+        i += 1
+        movie_one_length = movie_lengths[1]
+
+    return False
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_not_using_first_movie(self):
+        result = can_two_movies_fill_flight([4, 3, 2], 5)
+        self.assertTrue(result)
+
+    def test_multiple_movies_shorter_than_flight(self):
+        result = can_two_movies_fill_flight([5, 6, 7, 8], 9)
+        self.assertFalse(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)
