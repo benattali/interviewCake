9e327ffbc55a169ed4a1955034d1d5afb0c4f606
Ben Attali <ben.attali8@gmail.com>
2021-01-24 16:33:32 -0500

    add comments

--- hash maps/top_scores.py
+++ hash maps/top_scores.py
@@ -5,13 +5,19 @@ def sort_scores(unsorted_scores: list[int], highest_possible_score: int) -> list
     all_possible_scores = {}
     sorted_scores = []
 
+    # create a dictionary with all possible scores
     for i in range(highest_possible_score + 1):
         all_possible_scores[i] = 0
 
+    # incremenet the value of each score in the dictionary
     for score in unsorted_scores:
         all_possible_scores[score] += 1
     
+    # iterate through the socre ranges in reverse
     for i in range(highest_possible_score, -1, -1):
+        # if the score value is 0 (i.e that score doesnt exist)
+            # continue to the next score
+        # otherwise add the score to the final list, as many times as it appears
         if all_possible_scores[i] == 0:
             continue
         
